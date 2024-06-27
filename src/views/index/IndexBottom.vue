2fb7a36ec87fc73935b6a60d2ab1e3b3ee207646
mason <mimengc@163.com>
2024-06-28 00:26:06 +0800

    11

--- src/views/index/IndexBottom.vue
+++ src/views/index/IndexBottom.vue
@@ -1,47 +1,17 @@
 <template>
-    <div class="py-4 px-4 mx-0 mt-8 lg:mx-8">
-        <div class="grid justify-content-between">
-            <div class="col-12 md:col-2" style="margin-top: -1.5rem">
-                <!-- <a @click="smoothScroll('#home')" class="flex flex-wrap align-items-center justify-content-center md:justify-content-start md:mb-0 mb-3 cursor-pointer"> -->
-                    <!-- <img :src="logoUrl" alt="footer sections" width="50" height="50" class="mr-2" /> -->
-                    <!-- <h4 class="font-medium text-3xl text-900">SAKAI</h4> -->
-                <!-- </a> -->
-            </div>
-
-            <div class="col-12 md:col-10 lg:col-7">
-                <div class="grid text-center md:text-left">
-                    <div class="col-12 md:col-3">
-                        <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Company</h4>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">About Us</a>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">News</a>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Investor Relations</a>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Careers</a>
-                        <a class="line-height-3 text-xl block cursor-pointer text-700">Media Kit</a>
-                    </div>
-
-                    <div class="col-12 md:col-3 mt-4 md:mt-0">
-                        <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Resources</h4>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Get Started</a>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Learn</a>
-                        <a class="line-height-3 text-xl block cursor-pointer text-700">Case Studies</a>
-                    </div>
-
-                    <div class="col-12 md:col-3 mt-4 md:mt-0">
-                        <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Community</h4>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Discord</a>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Events<img src="/demo/images/landing/new-badge.svg" class="ml-2" /></a>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">FAQ</a>
-                        <a class="line-height-3 text-xl block cursor-pointer text-700">Blog</a>
-                    </div>
-
-                    <div class="col-12 md:col-3 mt-4 md:mt-0">
-                        <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Legal</h4>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Brand Policy</a>
-                        <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Privacy Policy</a>
-                        <a class="line-height-3 text-xl block cursor-pointer text-700">Terms of Service</a>
-                    </div>
-                </div>
-            </div>
+    <div class="py-4 mx-0 mt-1  flex justify-content-center">
+        <div class="contact_main">
+            <p class="text-center">
+                本站声明：本网资讯仅供体育爱好者浏览、购买中国足彩参考之用。任何人不得用于非法用途，否则责任自负。本网所登载广告均为广告客户的个人意见及表达方式，和本网无任何关系。链接的广告不得违反国家法律规定，如有违者，本网有权随时予以删除，并保留与有关部门合作追究的权利。使用本网站必须遵守本网址用户协议。
+            </p>
+            <p class="text-center">
+                <a href="https://beian.miit.gov.cn">豫ICP备2024074835号-1</a>
+            </p>
         </div>
     </div>
 </template>
+<style scoped>
+.contact_main {
+    width: 992px;
+}
+</style>
--- src/views/index/IndexContact.vue
+++ src/views/index/IndexContact.vue
@@ -1,14 +1,26 @@
 <template>
-    <div id="pricing" class="py-4 px-4 lg:px-8 my-2 md:my-4">
-        <div class="flex flex-row justify-content-between">
-            <div class="flex-column align-items-center">
-                <img style="width: 200px; height: auto;" src="/demo/bf/qq.png"/>
-                <p class="text-center">学习交流请加qq群</p>
-            </div>
-            <div class="flex-column align-items-center">
-                <img style="width: 200px; height: auto;" src="/demo/bf/wechat.png"/>
-                <p class="text-center">付费问题请联系客服微信</p>
+    <div class="surface-ground flex justify-content-center main_h_screen min-w-screen overflow-hidden">
+        <div class="contact_main mt-6">
+            <div id="pricing" class="py-4 px-4 lg:px-8 my-2 md:my-4">
+                <div class="flex flex-row justify-content-between">
+                    <div class="flex-column align-items-center">
+                        <img style="width: 200px; height: auto;" src="/demo/bf/qq.png"/>
+                        <p class="text-center">学习交流请加qq群</p>
+                    </div>
+                    <div class="flex-column align-items-center">
+                        <img style="width: 200px; height: auto;" src="/demo/bf/wechat.png"/>
+                        <p class="text-center">付费问题请联系客服微信</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
+<style scoped>
+.contact_main {
+    width: 992px;
+}
+.main_h_screen {
+    min-height: 70vh;
+}
+</style>
