fe8f4d9ddb336e6d228e61b89e72952ec94793d5
cai mimeng <mimengc@163.com>
2024-11-29 15:37:46 +0800

    1

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -62,7 +62,7 @@ const isOutsideClicked = (event) => {
     <div class="layout-topbar">
         <router-link to="/" class="layout-topbar-logo">
             <img :src="logoUrl" alt="logo" />
-            <span>SAKAI</span>
+            <span>后台管理</span>
         </router-link>
 
         <button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
